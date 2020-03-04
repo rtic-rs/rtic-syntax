251104fcfbe61db74e282c1711554eae2dbbf321
Per <Per Lindgren>
2020-03-04 14:55:22 +0100

    task_local and lock_free resources

--- src/ast.rs
+++ src/ast.rs
@@ -156,6 +156,15 @@ pub struct IdleArgs {
     pub(crate) _extensible: (),
 }
 
+/// Resource properties
+#[derive(Debug)]
+pub struct ResourceProperties {
+    /// a task local resource
+    pub task_local: bool,
+    /// a lock free (exclusive resource)
+    pub lock_free: bool,
+}
+
 /// An early (compile time initialized) resource
 #[derive(Debug)]
 pub struct Resource {
@@ -183,6 +192,9 @@ pub struct LateResource {
 
     /// The type of this resource
     pub ty: Box<Type>,
+
+    /// Resource properties
+    pub properties: ResourceProperties,
 
     pub(crate) _extensible: (),
 }
