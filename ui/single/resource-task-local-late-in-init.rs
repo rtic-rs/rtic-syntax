16b60ae1dd1361870712a351c75c6d042999e604
Henrik Tjäder <henrik@tjaders.com>
2020-10-13 16:53:46 +0000

    Task_local resource (late resource) in init

---
+++ ui/single/resource-task-local-late-in-init.rs
@@ -0,0 +1,22 @@
+#![no_main]
+
+#[mock::app]
+mod app {
+    #[resources]
+    struct Resources {
+        #[task_local]
+        x: u32,
+    }
+
+    #[init(resources = [x])]
+    fn init(_: init::Context) -> init::LateResources {
+        init::LateResources {}
+    }
+
+    #[task(
+                resources = [x],
+            )]
+    fn foo(c: foo::Context) {
+        c.resources.x += 1;
+    }
+}
